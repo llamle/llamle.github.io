981715e4a90f482300d5ebeb0e340713c6c987d5
Leon Lamle <llamle@gmail.com>
2015-06-04 11:15:51 -0400

    game1

---
+++ scripts/game.js
@@ -0,0 +1,16 @@
+console.log('Game is loaded...');
+
+var Game = {
+    empty: null,
+    player: "X",
+    opponent: "O",
+    board: [[null,null,null],
+            [null,null,null],
+            [null,null,null]],
+
+    playerName: "",
+    opponentName: "",
+
+    
+
+}
